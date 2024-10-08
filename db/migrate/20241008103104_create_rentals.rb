56a61d509e82150fa00332d0398bdb333476f89a
Rehyan Rhoden <rehyan92@hotmail.co.uk>
2024-10-08 14:08:53 +0100

    completed rental model validations

--- db/migrate/20241008103104_create_rentals.rb
+++ db/migrate/20241008103104_create_rentals.rb
@@ -2,7 +2,7 @@ class CreateRentals < ActiveRecord::Migration[7.1]
   def change
     create_table :rentals do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :vehicle, null: false, foreign_key: true
+      # t.references :vehicle, null: false, foreign_key: true
       t.date :hire_start_date
       t.date :hire_end_date
       t.string :duration
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_07_194327) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_08_103104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rentals", force: :cascade do |t|
+    t.bigint "user_id"
+    t.date "hire_start_date"
+    t.date "hire_end_date"
+    t.string "duration"
+    t.decimal "charge"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,4 +48,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_07_194327) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rentals", "users"
 end
